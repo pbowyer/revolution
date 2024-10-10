fac0dfcdc76566981930f717dfc42d2c32689360
Peter Bowyer <peter@mapledesign.co.uk>
2024-10-10 20:51:54 +0100

    And a simplified version which works!
    Fenom doesn't run when I thought it did, so Fenom doesn't interfere if the chunk is parsed first

--- core/components/twig/README.txt
+++ core/components/twig/README.txt
@@ -1,3 +1,20 @@
+This is not an ambitious integration.
+
+It focuses on chunks and doesn't try to get template inheritance working in the normal Twig way.
+It exposes the entire raw MODX object.
+It supports Twig inside resources, but isn't hoisting variables from their subcomponents up to it. So you can only use stuff that's actually available in the template and not, for example, loop through stuff that was used in a chunk. Which I think makes sense, but right now I can't think why.
+
+I expect you to use normal placeholders everywhere e.g. `[[++site_url]]`, but later on I will expose these to Twig too.
+
+There is no ContentBlocks integration yet.
+
+This Twig implementation is compatible with Fenom because it passes Twig first and Fenom runs at the last possible moment over the entire template in a single pass. While they have a speed advantage, I have the advantage of getting hold of all parameters passed to chunks and to chunks from snippets.
+
+Twig is a lot more forgiving than Fenom and will cope with missing tags and an awful lot of JSON and JavaScript in the template.
+
+
+------------------------------------------------------
+
 Add a Namespace to load the bootstrap.php file
 Name: twig
 Path: {core_path}components/twig/
--- core/components/twig/src/Proxy/modChunkTwig.php
+++ core/components/twig/src/Proxy/modChunkTwig.php
@@ -5,39 +5,23 @@ namespace Boffinate\Twig\Proxy;
 
 use Boffinate\Twig\Twig;
 use MODX\Revolution\modChunk;
-use MODX\Revolution\modElement;
 use ReflectionClass;
 
+/**
+ * A pain we have to extend the whole of modChunk, but MODX has some `instanceof` checks we need to pass.
+ * If only there was an interface to implement :)
+ */
 class modChunkTwig extends modChunk
 {
-    public mixed $processProperties = null;
-
     public function __construct(private modChunk $wrappedClass, private Twig $twig)
     {
         parent::__construct($twig->modx);
     }
 
-    /**
-     * This method is the reason the class exists and I'm jumping through all these steps.
-     * I can't change the class the CMS creates (modChunk).
-     * I can get it and wrap it in order to add this method to support Twig
-     * I can pass modChunkTwig around in place of the intercepted modChunk
-     * BUT... I can't pass the `instanceof modChunk` checks like this
-     */
     public function process($properties = null, $content = null)
     {
-        // We have to process Twig before Fenom which is why this comes first and duplicates some setup
-        $reflectionMethod = new \ReflectionMethod(get_parent_class(get_parent_class($this->wrappedClass)),
-            'process');
-        echo $reflectionMethod->invoke($this->wrappedClass);
-        if (is_string($this->wrappedClass->getContent())) {
-            $parsedProperties = $this->wrappedClass->getProperties($properties);
-            $twigRenderedContent = $this->twig->renderString($this->wrappedClass->getContent(), (array)$parsedProperties);
-            $this->wrappedClass->_content = $twigRenderedContent;
-        }
-        //$this->processProperties = $properties;
         $response = $this->wrappedClass->process($properties, $content);
-        //$this->processProperties = null;
+        $response = $this->twig->renderString($response, (array)$this->wrappedClass->_properties);
         return $response;
     }
 
